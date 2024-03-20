0b4fe6b331f23e226b71fd4ced9075d8b53783e7
Jonathan Walker <w@lker.dev>
2024-03-20 17:06:56 +0900

    profile

---
+++ src/challenges/profile-card-2.jsx
@@ -0,0 +1,73 @@
+const domNode = document.getElementById('app');
+const root = ReactDOM.createRoot(domNode);
+
+
+const options = [
+  "Login",
+  "Join",
+  "Change Language",
+  "Image and Video API",
+  "Apps and Plugins",
+  "FAQ",
+  "Partnerships",
+  "Inprint and Terms",
+]
+
+function Menu() {
+  return(
+    <menu className="absolute rounded p-2 bg-purple-800 text-yellow-100">
+      {options.map(option => 
+      <li className="p-1 cursor-pointer hover:bg-purple-600 rounded">
+	{option}
+      </li>
+	)
+      }
+    </menu>
+  ) 
+}
+
+function CardOptions({symbol}) {
+  const [menu, setMenu] = React.useState(false);
+
+  function handleClick() {
+    setMenu(!(menu))
+  }
+
+  return(
+    <div>
+      <span className="material-symbols-outlined -translate-y-2 border border-purple-800 rounded min-w-8 aspect-square flex items-center justify-center cursor-pointer 
+      hover:bg-yellow-500 ">
+	<button onClick={handleClick}>
+	  {symbol}
+	</button>
+      </span>
+      {(symbol==="more_horiz" && menu) ? <Menu/> : null}
+      </div>
+  )
+}
+
+function Card() {
+  return(
+    <div className="space-y-4 bg-yellow-300 border-purple-900 rounded border-4 text-purple-800 p-4 shadow-lg shadow-purple-500 flex flex-col justify-center items-center">
+      <img src="" className="rounded-full bg-white border-2 border-purple-800 aspect-square min-w-32"/>
+      <h1 className="font-semibold text-lg">Jonathan Walker</h1>
+      <p className="italic">Aspiring to Inspire. Inspired to Aspire</p>
+      <div className="flex space-x-8">
+	<CardOptions symbol="person"/>      
+	<CardOptions symbol="edit"/>    
+	<CardOptions symbol="more_horiz"/>      
+      </div>
+    </div>
+
+  )
+}
+
+function Home() {
+  return(
+    <div className="bg-yellow-100 min-h-screen flex justify-center items-center">
+      <Card/>    
+    </div>
+  )
+}
+
+root.render(<Home/>);
