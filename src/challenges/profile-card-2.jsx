066f94ae64a0b1f9913a075c5377880acdb83f29
Jonathan Walker <w@lker.dev>
2024-03-20 17:14:32 +0900

    profile

--- src/challenges/profile-card-2.jsx
+++ src/challenges/profile-card-2.jsx
@@ -13,15 +13,18 @@ const options = [
   "Inprint and Terms",
 ]
 
-function Menu() {
+function Menu({handleClick}) {
+
   return(
     <menu className="absolute rounded p-2 bg-purple-800 text-yellow-100">
-      {options.map(option => 
-      <li className="p-1 cursor-pointer hover:bg-purple-600 rounded">
-	{option}
-      </li>
-	)
-      }
+      <button  onClick={handleClick}>
+	{options.map(option => 
+	<li className="p-1 cursor-pointer hover:bg-purple-600 rounded">
+	  {option}
+	</li>
+	  )
+	}
+      </button>
     </menu>
   ) 
 }
@@ -41,7 +44,7 @@ function CardOptions({symbol}) {
 	  {symbol}
 	</button>
       </span>
-      {(symbol==="more_horiz" && menu) ? <Menu/> : null}
+      {(symbol==="more_horiz" && menu) ? <Menu handleClick={handleClick}/> : null}
       </div>
   )
 }
@@ -49,10 +52,10 @@ function CardOptions({symbol}) {
 function Card() {
   return(
     <div className="space-y-4 bg-yellow-300 border-purple-900 rounded border-4 text-purple-800 p-4 shadow-lg shadow-purple-500 flex flex-col justify-center items-center">
-      <img src="" className="rounded-full bg-white border-2 border-purple-800 aspect-square min-w-32"/>
+      <img src="https://raw.githubusercontent.com/lker-dev/icodethis.com/main/src/assets/jonathanwalker.jpg" className="rounded-full bg-white border-2 border-purple-800 aspect-square w-32"/>
       <h1 className="font-semibold text-lg">Jonathan Walker</h1>
       <p className="italic">Aspiring to Inspire. Inspired to Aspire</p>
-      <div className="flex space-x-8">
+      <div className="flex space-x-8 pt-8">
 	<CardOptions symbol="person"/>      
 	<CardOptions symbol="edit"/>    
 	<CardOptions symbol="more_horiz"/>      
